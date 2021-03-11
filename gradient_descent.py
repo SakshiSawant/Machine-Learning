09733954b087a0200780717a65fcf6d386c2c03e
SakshiSawant <56949381+SakshiSawant@users.noreply.github.com>
2021-03-11 23:03:27 +0530

    Update gradient_descent.py

--- gradient_descent.py
+++ gradient_descent.py
@@ -8,8 +8,8 @@ def gradient_descent(x,y):
     for i in range(iterations):
         y_predicted = m_curr*x + b_curr
         cost = (1/n) * sum([val**2 for val in (y - y_predicted)])
-        md = -(2/n)*sum(x*(y-y_predicted))
-        bd = -(2/n)*sum(y-y_predicted)
+        md = -(2/n)*sum(x*(y-y_predicted))  #m derivative
+        bd = -(2/n)*sum(y-y_predicted)      #b derivative
         m_curr = m_curr - learning_rate*md
         b_curr = b_curr - learning_rate*bd
         print("m {}, b {}, cost {} iteration {}".format(m_curr,b_curr,cost,i))
